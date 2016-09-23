a8bcd28d31da83b8a05616e671e16d3cbad2f2fa
sourabhdattawad <sourabhdattawad@gmail.com>
2016-09-24 01:05:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
